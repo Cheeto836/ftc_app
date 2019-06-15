a08f54426b4cf6e8e5199e252e88380d848a7fb3
Cheeto836 <mprussell836@gmail.com>
2019-06-15 15:02:51 -0400

    added empty classes for use in kiwi bot testing, removed old code from kiwi bot testing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Logger.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Logger implements Runnable
+{
+    public Logger()
+    {
+    }
+    public void run()
+    {
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sensors.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+
+public class Sensors implements Runnable
+{
+    public Sensors()
+    {
+    }
+    public void run()
+    {
+    }
+}
